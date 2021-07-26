652d8a94027744be8328f08ce940936a4ff9add6
Thibault Herreman <thibault.herreman@gmail.com>
2021-07-26 18:08:23 +0200

    debut screen map invite

--- PageComponents/InvitedMapScreen.js
+++ PageComponents/InvitedMapScreen.js
@@ -1,25 +1,77 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, StatusBar, View } from 'react-native';
 import { connect } from 'react-redux';
-
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+import ButtonElement from '../lib/ButtonElement';
+import SearchBarElement from '../lib/SearchBarElement';
+import { colors } from '../lib/colors';
 
 function InvitedMapScreen(props) {
 
+    const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
+
+    useEffect(() => {
+        async function askPermissions() {
+          let { status } = await Location.requestForegroundPermissionsAsync();
+          //console.log('status', status);
+          if (status === 'granted') {
+            Location.watchPositionAsync({ distanceInterval: 20 },
+              (location) => {
+                //console.log(location);
+                setPosition({ latitude: location.coords.latitude, longitude: location.coords.longitude });
+              }
+            );
+          }
+        }
+        askPermissions();
+    
+    }, [position]);
+
     return (
-        <View style={styles.container}>
-            <Text>invitedMapScreen</Text>
-            <Text>{`${props.token}`}</Text>
-            <Button title="login" onPress={() => props.login("monsupertokenchercheenbdd")} />
-            <Button title="signOut" onPress={() => props.signOut()} />
-            <Button title="InvitedMapScreen"
-                onPress={() => props.navigation.navigate('InvitedMapScreen')} />
-            <Button title="InvitedEventDetail"
-                onPress={() => props.navigation.navigate('InvitedEventDetail')} />
-            <Button title="Login"
-                onPress={() => props.navigation.navigate('Login')} />
-            <Button title="SignUp"
-                onPress={() => props.navigation.navigate('SignUp')} />
-        </View>
+        <>
+            <View style={styles.contentSearchBar}>
+                <SearchBarElement placeholder="Où ? (adresse)" />
+                <SearchBarElement placeholder="Quand ? (date)" />
+            </View>
+            <MapView
+                style={styles.container}
+                provider={PROVIDER_GOOGLE}
+                initialRegion={{
+                    latitude: 48.866667,
+                    longitude: 2.333333,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            >
+                <Marker 
+                    coordinate={{ latitude: position.latitude, longitude:  position.longitude }}
+                    title="Hello"
+                    description="I am here"
+                    //image={require('./assets/custom_pin.png')}
+                />
+            </MapView>
+            <ButtonElement 
+                 typeButton='fullFat'
+                 text='Se connecter'
+                 onPress={() => props.navigation.navigate('Login')}
+            />
+        </>
+
+        // <View style={styles.container}>
+        //     <Text>invitedMapScreen</Text>
+        //     <Text>{`${props.token}`}</Text>
+        //     <Button title="login" onPress={() => props.login("monsupertokenchercheenbdd")} />
+        //     <Button title="signOut" onPress={() => props.signOut()} />
+        //     <Button title="InvitedMapScreen"
+        //         onPress={() => props.navigation.navigate('InvitedMapScreen')} />
+        //     <Button title="InvitedEventDetail"
+        //         onPress={() => props.navigation.navigate('InvitedEventDetail')} />
+        //     <Button title="Login"
+        //         onPress={() => props.navigation.navigate('Login')} />
+        //     <Button title="SignUp"
+        //         onPress={() => props.navigation.navigate('SignUp')} />
+        // </View>
     );
 }
 
@@ -45,7 +97,16 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        
     },
+    contentSearchBar: {
+        marginTop: StatusBar.currentHeight || 0,
+        backgroundColor: colors.primary,
+        display: 'flex',
+        alignItems: 'center',
+        paddingTop: 10,
+        paddingBottom: 20
+    }
 });
 
 export default connect(
