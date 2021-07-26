8679f885d48f8c5059c922c70e7bfcd6f9ab9881
Hugo Castarede <hugocastarede@hotmail.fr>
2021-07-26 12:41:51 +0200

    modification Search bar + ajout autocomplete

--- PageComponents/OnBoarding.js
+++ PageComponents/OnBoarding.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import SearchBarElement from '../lib/SearchBarElement';
+import AutoComplete from '../lib/AutoComplete'
 import { connect } from 'react-redux';
 import ButtonElement from '../lib/ButtonElement';
 import {colors} from '../lib/colors';
@@ -15,36 +16,6 @@ function OnBoarding(props) {
     return (
         <View style={styles.container}>
             <Text>OnBoarding</Text>
-
-            <ButtonElement 
-                text='Participer'
-                propStyles={{
-                    color: colors.white,
-                    bgColor: colors.secondary,
-                    fontSize: 14,
-                    fontWeight: 500,
-                    width: 154,
-                    height: 33,
-                    borderRadius: 5
-                }}
-                onPress={fctTest}
-            />
-
-            <ButtonElement 
-                propStyles={{
-                    color: colors.white,
-                    bgColor: colors.secondary,
-                    borderRadius: 100,
-                    width: 30,
-                    height: 30,
-                }}
-                icon={{
-                    type: 'AntDesign',
-                    name: 'arrowleft',
-                    size: 12
-                }}
-                onPress={fctTest}
-            />
 
             <Text>{`${props.token}`}</Text>
             <Button title="login" onPress={() => props.login("monsupertokenchercheenbdd")} />
