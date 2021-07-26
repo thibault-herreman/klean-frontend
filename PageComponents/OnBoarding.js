0e371808fc4d6ddb1f5b9cbe8dd68cc935b144a4
Clothilde Diot <diot.clothilde@gmail.com>
2021-07-26 13:09:13 +0200

    création premier input + installation font

--- PageComponents/OnBoarding.js
+++ PageComponents/OnBoarding.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { connect } from 'react-redux';
-import ButtonElement from '../lib/ButtonElement';
-import {colors} from '../lib/colors';
-
 
 function OnBoarding(props) {
 
@@ -14,37 +11,6 @@ function OnBoarding(props) {
     return (
         <View style={styles.container}>
             <Text>OnBoarding</Text>
-
-            <ButtonElement 
-                text='Participer'
-                propStyles={{
-                    color: colors.white,
-                    bgColor: colors.secondary,
-                    fontSize: 14,
-                    fontWeight: 500,
-                    width: 154,
-                    height: 33,
-                    borderRadius: 5
-                }}
-                onPress={fctTest}
-            />
-
-            <ButtonElement 
-                propStyles={{
-                    color: colors.white,
-                    bgColor: colors.secondary,
-                    borderRadius: 100,
-                    width: 30,
-                    height: 30,
-                }}
-                icon={{
-                    type: 'AntDesign',
-                    name: 'arrowleft',
-                    size: 12
-                }}
-                onPress={fctTest}
-            />
-
             <Text>{`${props.token}`}</Text>
             <Button title="login" onPress={() => props.login("monsupertokenchercheenbdd")} />
             <Button title="signOut" onPress={() => props.signOut()} />
