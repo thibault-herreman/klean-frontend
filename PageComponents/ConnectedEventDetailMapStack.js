0cb11baf907d17921a553dc7390c1ebf86cf2e3a
Clothilde Diot <diot.clothilde@gmail.com>
2021-07-27 10:32:49 +0200

    ajout bannière + modification typo

--- PageComponents/ConnectedEventDetailMapStack.js
+++ PageComponents/ConnectedEventDetailMapStack.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import { connect } from 'react-redux';
 
-import {Badges} from "../lib/badges.js";
+import {Badges} from "../lib/Badges.js";
 import ScreenTitles from '../lib/ScreenTitles.js';
+import ButtonElement from "../lib/ButtonElement";
+import {windowDimensions} from '../lib/windowDimensions.js';
+import {typography} from '../lib/typography.js';
 
 function ConnectedEventDetailMapStack(props) {
 
@@ -25,30 +28,37 @@ function ConnectedEventDetailMapStack(props) {
         <View style={styles.container}>
 
             <View>
-                <Image ></Image>
+                <ImageBackground style={styles.banner} source={require('../assets/imagesKlean/BannerCleanwalk.jpg')}>
+                    <ButtonElement style={styles.backButton} typeButton="back" />
+                    <ButtonElement style={styles.goButton} typeButton="go" />
+                </ImageBackground>
             </View>
 
-            <View>
-                <Text>Nettoyage de la plage de Santa Giulia</Text>
-                <Text>Corse du Sud</Text>
-                <Text>Début : Samedi 8 août 2021 à 11h30</Text>
-                <Text>Fin : Dimanche 9 août 2021 à 12h30</Text>
+            <View style={styles.generalInfoCleanwalk}>
+                <Text style={typography.h2}>Nettoyage de la plage de Santa Giulia</Text>
+                <Text style={typography.bodyLight}>Corse du Sud</Text>
+                <Text style={typography.bodyLight}>Début : Samedi 8 août 2021 à 11h30</Text>
+                <Text style={typography.bodyLight}>Fin : Dimanche 9 août 2021 à 12h30</Text>
             </View>
 
-            <View>
-                <Text>Description</Text>
-                <Text>Lorem ipsum Lorem ipsumLorem</Text>
+            <View style={styles.descriptionCleanwalk}>
+                <Text style={typography.h3}>Description</Text>
+                <View style={styles.cleanwakDescriptionContainer}>
+                    <Text style={typography.bodyLight}>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</Text> 
+                </View>
+                
             </View>
 
-            <View style={{alignItems: "center"}}>
+            <View style={styles.badges}>
                 <Badges type="green"/> 
-                <Badges type="orange"/>
+                <Badges type="green"/> 
+                <Badges type="green"/> 
+                <Badges type="green"/> 
             </View>
 
             <View>
                 <ScreenTitles titleType="secondary" 
                 title={"Participants"}/>
-
             </View>
 
         </View>
@@ -78,6 +88,48 @@ const styles = StyleSheet.create({
         // alignItems: 'center',
         // justifyContent: 'center',
     },
+    banner: {
+        width: windowDimensions.width,
+        height: 76,
+        marginTop: 29,
+        justifyContent: 'space-between',
+        paddingLeft: 17,
+        paddingRight: 17,
+        flexDirection: 'row',
+        alignItems: 'center',
+
+    },
+    backButton: {
+        position: 'absolute', 
+    },
+    goButton: {
+        position: 'absolute', 
+    },
+    generalInfoCleanwalk: {
+        marginTop: 11,
+        marginBottom: 11,
+        marginLeft: 18,
+    },
+    cleanwalkTitle: {
+        fontFamily: 'Lato_400Regular',
+        fontSize: 18,
+    },
+    descriptionCleanwalk: {
+        marginBottom: 11,
+        marginLeft: 18,
+    },
+    cleanwakDescriptionContainer: {
+        marginRight: 18,
+    },
+    badges: {
+        marginTop: 11,
+        marginBottom: 11,
+        marginLeft: 18,
+        marginRight: 18,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+    }
+
 });
 
 export default connect(
