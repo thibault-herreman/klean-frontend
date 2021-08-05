337c5b643f7b0cf94b28f69bc1e0d30042cb2747
Hugo Castarede <hugocastarede@hotmail.fr>
2021-08-05 16:30:42 +0200

    logbox

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import { LogBox } from 'react-native';
+LogBox.ignoreAllLogs()
+
 import { useFonts, Lato_100Thin, Lato_100Thin_Italic, Lato_300Light, Lato_300Light_Italic, Lato_400Regular, Lato_400Regular_Italic, Lato_700Bold, Lato_700Bold_Italic, Lato_900Black, Lato_900Black_Italic  } from '@expo-google-fonts/lato';
 import AppLoading from 'expo-app-loading';
 
